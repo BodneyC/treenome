fbb5a0ba84ece2c4e711786a3b4ebb72b0fee2a2
Benjamin Carrington <benjcarrington@gmail.com>
2018-07-26 18:02:15 +0100

    Stylistic change ( brackets )

--- src/GTree.C
+++ src/GTree.C
@@ -16,7 +16,7 @@
 #ifdef __MINGW64__
 #include <sstream>
 namespace mingw_fix {
-	template< typename T > std::string to_string(const T &val)
+	template< typename T > std::string to_string( const T &val )
 	{
 		std::ostringstream ss;
 		ss << val ;
@@ -29,9 +29,9 @@ namespace mingw_fix {
 namespace GTH {
 	std::vector<SeqRead> seqReads;
 
-	char retLabel(int label)
+	char retLabel( int label )
 	{
-		switch(label) {
+		switch( label ) {
 		case 0:
 			return 'A';
 		case 1:
@@ -45,26 +45,26 @@ namespace GTH {
 		}
 	}
 
-	//float getNewWeight(double curWeight, int64_t occs, char qual)
+	//float getNewWeight( double curWeight, int64_t occs, char qual )
 	//{
 	//	// Cumulative average: A_{n+1} = ((A_n + x_{n+1}) - A_n) / n + 1
-	//	return curWeight += ((static_cast<double>(qual) - curWeight) /
-	//			static_cast<double>(occs));
+	//	return curWeight += ( ( static_cast<double>( qual ) - curWeight ) /
+	//			static_cast<double>( occs ) );
 	//}
 
-	void removeDoubleEnding(std::string& doubleString)
+	void removeDoubleEnding( std::string& doubleString )
 	{
-		doubleString.erase(doubleString.find_last_not_of('0') + 1, std::string::npos);
-		doubleString.erase(doubleString.find_last_not_of('.') + 1, std::string::npos);
+		doubleString.erase( doubleString.find_last_not_of('0') + 1, std::string::npos );
+		doubleString.erase( doubleString.find_last_not_of('.') + 1, std::string::npos );
 	}
 	
 	template <typename T>
-	std::string valToString(T& val)
+	std::string valToString( T& val )
 	{
 #ifdef __MINGW64__
-		return mingw_fix::to_string(val);
+		return mingw_fix::to_string( val );
 #else
-		return std::to_string(val);
+		return std::to_string( val );
 #endif /*__MINGW64__*/
 	}
 }
@@ -72,44 +72,44 @@ namespace GTH {
 /** ------------- GTree Cons and Dees -------------- **/
 template <typename T>
 GTree<T>::GTree(): 
-	root(nullptr), head(0), nNodes(0), 
-	basePaths(""), occuPaths(""), treeString("")
+	root( nullptr ), head( 0 ), nNodes( 0 ), 
+	basePaths( "" ), occuPaths( "" ), treeString( "" )
 { 
-	omp_init_lock(&lock);
+	omp_init_lock( &lock );
 }
 
 /** ------------- - Helper Functions --------------- **/
 template <typename T>
-void GTree<T>::updateWeight(T* node, char qual)
+void GTree<T>::updateWeight( T* node, char qual )
 {
 	double newWeight, curWeight = node->weight;
-	double pBQual = GTH::phredQuals[static_cast<int>(qual)];
+	double pBQual = GTH::phredQuals[static_cast<int>( qual )];
 	node->occs++;
 	do {
 		newWeight = curWeight + pBQual;
-	} while(!(node->weight.compare_exchange_weak(curWeight, newWeight)));
+	} while( !( node->weight.compare_exchange_weak( curWeight, newWeight ) ) );
 }
 
 template <typename T>
-short GTree<T>::countChildren(T* node)
+short GTree<T>::countChildren( T* node )
 {
 	short children = 0;
 
-	for(int i = 0; i < NBASES; i++)
-		if(node->subnodes[i])
+	for( int i = 0; i < NBASES; i++ )
+		if( node->subnodes[i] )
 			children++;
 
 	return children;
 }
 
 template <typename T>
-signed short GTree<T>::mostOccs(T* node)
+signed short GTree<T>::mostOccs( T* node )
 {
 	short ind = -1;
 	int64_t occCnt = 0;
-	for(int i = 0; i < NBASES; i++) {
+	for( int i = 0; i < NBASES; i++ ) {
 		T* tmpNode = node->subnodes[i];
-		if(tmpNode && tmpNode->occs > occCnt) {
+		if( tmpNode && tmpNode->occs > occCnt ) {
 			occCnt = tmpNode->occs;
 			ind = i;
 		}
@@ -118,36 +118,36 @@ signed short GTree<T>::mostOccs(T* node)
 }
 
 template <typename T>
-void GTree<T>::followPath(T* node, short ind, std::string &sequence)
+void GTree<T>::followPath( T* node, short ind, std::string &sequence )
 {
 	short children;
 	do {
-		children = countChildren(node);
-		sequence += GTH::retLabel(ind);
-		ind = mostOccs(node);
+		children = countChildren( node );
+		sequence += GTH::retLabel( ind );
+		ind = mostOccs( node );
 		node->occs--;
-		if(ind == -1)
+		if( ind == -1 )
 			return;
 		node = node->subnodes[ind];
-	} while(children);
+	} while( children );
 }
 
 /** --------------- Genome Creation ---------------- **/
 template <typename T>
-void GTree<T>::addToSeq(uint64_t offset, std::string &sequence)
+void GTree<T>::addToSeq( uint64_t offset, std::string &sequence )
 {
 	T* node = root;
 	short ind = 0;
 	uint32_t i, j, seqLength = sequence.length() - offset;
 	T** path = new T*[seqLength];
 
-	for(i = 0; i < seqLength; i++)
+	for( i = 0; i < seqLength; i++ )
 		path[i] = nullptr;
 
 	// End of current sequence	
-	for(i = offset + 1, j = 0; i < sequence.length(); i++, j++) {
-		ind = BASE_IND(sequence[i]);
-		if(node->subnodes[ind] && countChildren(node->subnodes[ind])) {
+	for( i = offset + 1, j = 0; i < sequence.length(); i++, j++ ) {
+		ind = BASE_IND( sequence[i] );
+		if( node->subnodes[ind] && countChildren( node->subnodes[ind] ) ) {
 			path[j] = node;
 			node = node->subnodes[ind];
 		} else {
@@ -156,14 +156,14 @@ void GTree<T>::addToSeq(uint64_t offset, std::string &sequence)
 		}
 	}
 
-	ind = mostOccs(node);
-	if(ind != -1) {
-		followPath(node->subnodes[ind], ind, sequence);
+	ind = mostOccs( node );
+	if( ind != -1 ) {
+		followPath( node->subnodes[ind], ind, sequence );
 		
 		// Only if something is contributed to the sequence should the 
 		// occurences be lowered
-		for(i = 0; i < seqLength; i++)
-			if(path[i]) 
+		for( i = 0; i < seqLength; i++ )
+			if( path[i] ) 
 				path[i]->occs--;
 	}
 
@@ -172,32 +172,32 @@ void GTree<T>::addToSeq(uint64_t offset, std::string &sequence)
 
 /** ---------------- Tree Storage ------------------ **/
 template <typename T>
-std::string GTree<T>::storeTree(short label) {
-	treeString += GTH::valToString(nNodes) + '\n';
+std::string GTree<T>::storeTree( short label ) {
+	treeString += GTH::valToString( nNodes ) + '\n';
 	std::string val[2] = {
-		GTH::valToString(root->occs),
-		GTH::valToString(root->weight)
+		GTH::valToString( root->occs ),
+		GTH::valToString( root->weight )
 	};
-	GTH::removeDoubleEnding(val[1]);
-	treeString += GTH::retLabel(label);
+	GTH::removeDoubleEnding( val[1] );
+	treeString += GTH::retLabel( label );
 	treeString += ':' + val[0] + ':' + val[1] + ';';
-	storeTree(root);
+	storeTree( root );
 	return treeString;
 }
 
 template <typename T>
-void GTree<T>::storeTree(T* node)
+void GTree<T>::storeTree( T* node )
 {
-	for(int i = 0; i < NBASES; i++) {
-		if(node->subnodes[i]) {
-			treeString += GTH::retLabel(i);
+	for( int i = 0; i < NBASES; i++ ) {
+		if( node->subnodes[i] ) {
+			treeString += GTH::retLabel( i );
 			std::string val[2] = {
-				GTH::valToString(node->subnodes[i]->occs),
-				GTH::valToString(node->subnodes[i]->weight)
+				GTH::valToString( node->subnodes[i]->occs ),
+				GTH::valToString( node->subnodes[i]->weight )
 			};
-			GTH::removeDoubleEnding(val[1]);
+			GTH::removeDoubleEnding( val[1] );
 			treeString += ':' + val[0] + ':' + val[1] + ';';
-			storeTree(node->subnodes[i]);
+			storeTree( node->subnodes[i] );
 		}
 	}
 	treeString += ',';
@@ -205,33 +205,33 @@ void GTree<T>::storeTree(T* node)
 
 /** ---------------- Path Printing ----------------- **/
 template <typename T>
-void GTree<T>::printAllPaths(T* node, int len, short label)
+void GTree<T>::printAllPaths( T* node, int len, short label )
 {
-    if(!node)
+    if( !node )
         return;
 
-	occuPaths.erase(len, occuPaths.length());
-	std::string val = GTH::valToString(node->occs);
+	occuPaths.erase( len, occuPaths.length() );
+	std::string val = GTH::valToString( node->occs );
     occuPaths += val;
 	occuPaths += "-";
-	basePaths.erase(len, basePaths.length());
-	basePaths += GTH::retLabel(label);
-	for(uint32_t i = 0; i < val.length(); i++)
+	basePaths.erase( len, basePaths.length() );
+	basePaths += GTH::retLabel( label );
+	for( uint32_t i = 0; i < val.length(); i++ )
 		basePaths += '-';
 	len += val.length() + 1;
 	bool check = 0;
-	for(int i = 0; i < NBASES; i++)
-		if(node->subnodes[i])
+	for( int i = 0; i < NBASES; i++ )
+		if( node->subnodes[i] )
 			check = true;
-    if(!check) {
-		occuPaths.erase(occuPaths.length() - 1);
+    if( !check ) {
+		occuPaths.erase( occuPaths.length() - 1 );
 		std::cout << occuPaths << ": EOS" << std::endl;
-		basePaths.erase(basePaths.length() - 1);
+		basePaths.erase( basePaths.length() - 1 );
 		std::cout << basePaths << ": EOS\n" << std::endl;
         return;
     }
-	for(int i = 0; i < NBASES; i++)
-		printAllPaths(node->subnodes[i], len, i);
+	for( int i = 0; i < NBASES; i++ )
+		printAllPaths( node->subnodes[i], len, i );
 }
 
 template class GTree<Node>;
